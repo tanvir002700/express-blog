de23638e2358e02ce1a657175630c2f8e95d6fef
tanvir hasan <tanvir002700@gmail.com>
2018-08-09 23:16:05 +0600

    add registration page

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/register', function(req, res, next) {
+router.get('/new', function(req, res, next) {
     res.render('users/new', { csrfToken: req.csrfToken() });
+});
+
+router.post('/create', function(req, res, next) {
+    console.log('create user');
+    res.redirect('/');
 });
 
 module.exports = router;

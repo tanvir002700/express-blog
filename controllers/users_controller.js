046582496b60fb32358d7b0878779f95aa2b902f
tanvir hasan <tanvir002700@gmail.com>
2018-08-14 23:43:31 +0600

    add login post method

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -10,7 +10,7 @@ router.get('/new', function(req, res, next) {
 
 router.post('/create', function(req, res, next) {
 
-    User.find_by_email(req.body, function(err, result) {
+    User.find_by_email(req.body.email, function(err, result) {
         if(result.rows.length || err) {
             res.redirect('/');
         } else {
@@ -26,7 +26,7 @@ router.get('/login', function(req, res, next) {
 });
 
 passport.use(new LocalStrategy(function(email, password, done) {
-    User.find_by_email(req.body, function(err, result) {
+    User.find_by_email(email, function(err, result) {
         if(err) throw err;
         if(result.rows.legth) {
             return done(null, false, { message: 'Unknown User' });
@@ -37,14 +37,25 @@ passport.use(new LocalStrategy(function(email, password, done) {
             if(isMatch) {
                 return done(null, user);
             } else {
-                return done(null, false, message: 'Invalid Password');
+                return done(null, false, { message: 'Invalid Password' });
             }
         });
     });
 }));
 
-router.post('/login', function(req, res, next) {
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+});
 
+passport.deserializeUser(function(id, done) {
+    User.find_by_id(id, function(err, result) {
+        done(err, result.rows[0]);
+    });
+});
+
+router.post('/login',
+    passport.authenticate('local', { successRedirect: '/', failureRedirect: '/users/login', failureFlash: true }),
+    function(req, res) {
 });
 
 module.exports = router;

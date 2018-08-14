9dc6d5db9396242292641ccb9769d0ec63342e49
tanvir hasan <tanvir002700@gmail.com>
2018-08-15 00:21:50 +0600

    change db schema

--- db/schema.js
+++ db/schema.js
@@ -1,7 +1,7 @@
 const client = require('./client');
 
 queries = [
-    'CREATE TABLE IF NOT EXISTS users(id uuid, email text, name text, password text, PRIMARY KEY(id, email))',
+    'CREATE TABLE IF NOT EXISTS users(id uuid, email text, username text, password text, PRIMARY KEY(id, email, username))',
     'CREATE TABLE IF NOT EXISTS posts(id uuid PRIMARY KEY, title text, description text)'
 ];
 

2c5c58e12c271610450ccd157bd9396a08ca48bd
tanvir hasan <tanvir002700@gmail.com>
2018-08-14 22:26:40 +0600

    add local strategy method

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
 const User = require('../models/user');
 
 router.get('/new', function(req, res, next) {
@@ -21,6 +23,28 @@ router.post('/create', function(req, res, next) {
 
 router.get('/login', function(req, res, next) {
     res.render('users/login', { csrfToken: req.csrfToken() });
+});
+
+passport.use(new LocalStrategy(function(email, password, done) {
+    User.find_by_email(req.body, function(err, result) {
+        if(err) throw err;
+        if(result.rows.legth) {
+            return done(null, false, { message: 'Unknown User' });
+        }
+        user = result.rows[0];
+        User.compare_password(password, user.password, function(err, isMatch) {
+            if(err) throw err;
+            if(isMatch) {
+                return done(null, user);
+            } else {
+                return done(null, false, message: 'Invalid Password');
+            }
+        });
+    });
+}));
+
+router.post('/login', function(req, res, next) {
+
 });
 
 module.exports = router;

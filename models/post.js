22e5aa7ca63a234fe7fdc289820867bde96e14c8
tanvir hasan <tanvir002700@gmail.com>
2018-08-16 21:31:47 +0600

    update posts query consts

--- models/post.js
+++ models/post.js
@@ -1,9 +1,10 @@
 const client = require('../db/client');
-const all_posts_query = 'SELECT * FROM posts';
-const create_post_query = 'INSERT INTO posts(id, title, description) VALUES(now(), ?, ?)';
-const find_by_id_query = 'SELECT * FROM posts WHERE id=?';
-const update_post_query = 'UPDATE posts SET title=?,description=? WHERE id=?';
-const delete_post_query = 'DELETE FROM posts WHERE id=?';
+
+const ALL_POSTS_QUERY = 'SELECT * FROM posts';
+const CREATE_POST_QUERY = 'INSERT INTO posts(id, title, description) VALUES(now(), ?, ?)';
+const FIND_BY_ID_QUERY = 'SELECT * FROM posts WHERE id=?';
+const UPDATE_POST_QUERY = 'UPDATE posts SET title=?,description=? WHERE id=?';
+const DELETE_POST_QUERY = 'DELETE FROM posts WHERE id=?';
 
 const dbResponseCallback = (err, res) => {
     this.callBack(err, res);
@@ -11,25 +12,25 @@ const dbResponseCallback = (err, res) => {
 
 module.exports.all = function(params, callBack) {
     this.callBack = callBack;
-    client.execute(all_posts_query, [], dbResponseCallback);
+    client.execute(ALL_POSTS_QUERY, [], dbResponseCallback);
 };
 
 module.exports.create = function(params, callBack) {
     this.callBack = callBack;
-    client.execute(create_post_query, [params.title, params.description], dbResponseCallback);
+    client.execute(CREATE_POST_QUERY, [params.title, params.description], dbResponseCallback);
 };
 
 module.exports.find_by_id = function(id, callBack) {
     this.callBack = callBack;
-    client.execute(find_by_id_query, [id], dbResponseCallback);
+    client.execute(FIND_BY_ID_QUERY, [id], dbResponseCallback);
 };
 
 module.exports.update = function(id, params, callBack) {
     this.callBack = callBack;
-    client.execute(update_post_query, [params.title, params.description, id], dbResponseCallback);
+    client.execute(UPDATE_POST_QUERY, [params.title, params.description, id], dbResponseCallback);
 };
 
 module.exports.delete = function(id, callBack) {
     this.callBack = callBack;
-    client.execute(delete_post_query, [id], dbResponseCallback);
+    client.execute(DELETE_POST_QUERY, [id], dbResponseCallback);
 };

8e8d5ccf6c086ab8e6f3861b0989ac34ec464702
tanvir hasan <tanvir002700@gmail.com>
2018-08-19 23:16:36 +0600

    separate isLoginIn method to middleware

--- controllers/index.js
+++ controllers/index.js
@@ -1,19 +1,13 @@
 var express = require('express');
 var router = express.Router();
-
-function isLoggedIn(req, res, next) {
-  if(req.isAuthenticated()) {
-    return next();
-  }
-  return res.redirect('users/login');
-}
+var auth = require('../middlewares/auth');
 
 /* GET home page. */
-router.get('/', isLoggedIn, function(req, res, next) {
+router.get('/', auth.isLoggedIn, function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.use('/posts', isLoggedIn, require('./posts_controller'));
+router.use('/posts', auth.isLoggedIn, require('./posts_controller'));
 router.use('/users', require('./users_controller'));
 
 module.exports = router;

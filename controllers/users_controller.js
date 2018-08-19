e7a3d9432c089f7bcdebd4603d11842f8836b5da
tanvir hasan <tanvir002700@gmail.com>
2018-08-19 23:27:56 +0600

    add local strategy method to auth middlewares

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,8 +1,7 @@
 var express = require('express');
 var router = express.Router();
-var passport = require('passport');
-var LocalStrategy = require('passport-local').Strategy;
 var User = require('../models/user');
+var auth = require('../middlewares/auth');
 
 router.get('/new', function(req, res, next) {
     res.render('users/new', { csrfToken: req.csrfToken() });
@@ -33,36 +32,9 @@ router.get('/login', function(req, res, next) {
     res.render('users/login', { csrfToken: req.csrfToken() });
 });
 
-passport.use(new LocalStrategy(function(username, password, done) {
-    User.find_by_username(username, function(err, result) {
-        if(err) throw err;
-        if(!result.rows.length) {
-            return done(null, false, { message: 'Unknown User' });
-        }
-        user = result.rows[0];
-        User.compare_password(password, user.password, function(err, isMatch) {
-            if(err) throw err;
-            if(isMatch) {
-                return done(null, user);
-            } else {
-                return done(null, false, { message: 'Invalid Password' });
-            }
-        });
-    });
-}));
-
-passport.serializeUser(function(user, done) {
-    done(null, user.id);
-});
-
-passport.deserializeUser(function(id, done) {
-    User.find_by_id(id, function(err, result) {
-        done(err, result.rows[0]);
-    });
-});
 
 router.post('/login',
-    passport.authenticate('local', { successRedirect: '/', failureRedirect: '/users/login', failureFlash: true }),
+    auth.passport.authenticate('local', { successRedirect: '/', failureRedirect: '/users/login', failureFlash: true }),
     function(req, res) {
 });
 
--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,3 +1,37 @@
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var User = require('../models/user');
+
+passport.use(new LocalStrategy(function(username, password, done) {
+    User.find_by_username(username, function(err, result) {
+        if(err) throw err;
+        if(!result.rows.length) {
+            return done(null, false, { message: 'Unknown User' });
+        }
+        user = result.rows[0];
+        User.compare_password(password, user.password, function(err, isMatch) {
+            if(err) throw err;
+            if(isMatch) {
+                return done(null, user);
+            } else {
+                return done(null, false, { message: 'Invalid Password' });
+            }
+        });
+    });
+}));
+
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+    User.find_by_id(id, function(err, result) {
+        done(err, result.rows[0]);
+    });
+});
+
+module.exports.passport = passport;
+
 module.exports.isLoggedIn = function(req, res, next) {
     if(req.isAuthenticated()) {
         return next();

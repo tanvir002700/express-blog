4bfe22e510e4dcbfa23e4276ac57fc1e95627a6e
tanvir hasan <tanvir002700@gmail.com>
2018-08-20 17:56:09 +0600

    remove unnecessary callback

--- models/post.js
+++ models/post.js
@@ -1,4 +1,5 @@
 const client = require('../db/client');
+var Base = require('./base');
 
 const ALL_POSTS_QUERY = 'SELECT * FROM posts';
 const CREATE_POST_QUERY = 'INSERT INTO posts(id, title, description) VALUES(now(), ?, ?)';
@@ -6,31 +7,27 @@ const FIND_BY_ID_QUERY = 'SELECT * FROM posts WHERE id=?';
 const UPDATE_POST_QUERY = 'UPDATE posts SET title=?,description=? WHERE id=?';
 const DELETE_POST_QUERY = 'DELETE FROM posts WHERE id=?';
 
-const dbResponseCallback = (err, res) => {
-    this.callBack(err, res);
-};
-
 module.exports.all = function(params, callBack) {
     this.callBack = callBack;
-    client.execute(ALL_POSTS_QUERY, [], dbResponseCallback);
+    client.execute(ALL_POSTS_QUERY, [], callBack);
 };
 
 module.exports.create = function(params, callBack) {
     this.callBack = callBack;
-    client.execute(CREATE_POST_QUERY, [params.title, params.description], dbResponseCallback);
+    client.execute(CREATE_POST_QUERY, [params.title, params.description], callBack);
 };
 
 module.exports.findById = function(id, callBack) {
     this.callBack = callBack;
-    client.execute(FIND_BY_ID_QUERY, [id], dbResponseCallback);
+    client.execute(FIND_BY_ID_QUERY, [id], callBack);
 };
 
 module.exports.update = function(id, params, callBack) {
     this.callBack = callBack;
-    client.execute(UPDATE_POST_QUERY, [params.title, params.description, id], dbResponseCallback);
+    client.execute(UPDATE_POST_QUERY, [params.title, params.description, id], callBack);
 };
 
 module.exports.delete = function(id, callBack) {
     this.callBack = callBack;
-    client.execute(DELETE_POST_QUERY, [id], dbResponseCallback);
+    client.execute(DELETE_POST_QUERY, [id], callBack);
 };

1d6d8f68e923005525bc570bdb54380fece1c955
tanvir hasan <tanvir002700@gmail.com>
2018-08-27 20:32:51 +0600

    add login validations

--- validations/user.js
+++ validations/user.js
@@ -1,6 +1,12 @@
 const { check } = require('express-validator/check');
 
 module.export.registrationValidations = [
-  check('email').not.isEmpty(),
-  check('username').not.isEmpty(),
+  check('email').not().isEmpty(),
+  check('username').not().isEmpty(),
+];
+
+module.export.loginValidations = [
+  check('username').not().isEmpty(),
+  check('password').not().isEmpty(),
+  check('password_confirmation').not().isEmpty()
 ];

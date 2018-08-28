89d36cd34343bd2249befb789549722d1d760e60
tanvir hasan <tanvir002700@gmail.com>
2018-08-29 02:19:01 +0600

    refactor user validation

--- validations/user.js
+++ validations/user.js
@@ -1,13 +1,24 @@
 const { check } = require('express-validator/check');
 
 module.exports.registrationValidations = [
-  check('email').not().isEmpty().withMessage('Email should not be empty.').isEmail().withMessage('Is not Valid Email'),
-  check('username').not().isEmpty(),
-  check('password').not().isEmpty(),
-  check('password_confirmation').not().isEmpty()
+  check('email').not()
+    .isEmpty()
+    .withMessage('Email should not be empty.')
+    .isEmail()
+    .withMessage('Is not Valid Email'),
+  check('username').not().isEmpty()
+    .withMessage('User should not be empty.'),
+  check('password').not().isEmpty()
+    .withMessage('Password should not be empty.'),
+  check('password_confirmation').not()
+    .isEmpty().withMessage('password confirmation should not be empty.'),
 ];
 
 module.exports.loginValidations = [
-  check('username').not().isEmpty(),
-  check('password').not().isEmpty(),
+  check('username').not()
+    .isEmpty()
+    .withMessage('username should not empty.'),
+  check('password').not()
+    .isEmpty()
+    .withMessage('password should not empty.'),
 ];

7daae1b4f978ecdabaa46f54c7dd25dc20edff59
tanvir hasan <tanvir002700@gmail.com>
2018-08-31 03:00:35 +0600

    refactor code

--- models/user.js
+++ models/user.js
@@ -3,24 +3,44 @@ const client = require('../db/client');
 const Query = require('../db/query');
 
 
-module.exports.create = function(params, callBack) {
-    const salt = bcrypt.genSaltSync(10);
-    const hash = bcrypt.hashSync(params.password, salt);
-    client.execute(Query.User.CREATE, [params.email, params.username, hash], callBack);
+module.exports.create = (params, callBack) => {
+  const salt = bcrypt.genSaltSync(10);
+  const hash = bcrypt.hashSync(params.password, salt);
+  client.execute(
+    Query.User.CREATE,
+    [
+      params.email,
+      params.username,
+      hash,
+    ],
+    callBack,
+  );
 };
 
-module.exports.findByEmail = function(email, callBack) {
-    client.execute(Query.User.FIND_BY_EMAIL, [email], callBack);
+module.exports.findByEmail = (email, callBack) => {
+  client.execute(
+    Query.User.FIND_BY_EMAIL,
+    [email],
+    callBack,
+  );
 };
 
-module.exports.findById = function(id, callBack) {
-    client.execute(Query.User.FIND_BY_ID, [id], callBack);
+module.exports.findById = (id, callBack) => {
+  client.execute(
+    Query.User.FIND_BY_ID,
+    [id],
+    callBack,
+  );
 };
 
-module.exports.findByUsername = function(username, callBack) {
-    client.execute(Query.User.FIND_BY_USERNAME, [username], callBack);
+module.exports.findByUsername = (username, callBack) => {
+  client.execute(
+    Query.User.FIND_BY_USERNAME,
+    [username],
+    callBack,
+  );
 };
 
-module.exports.comparePassword = function(candidatePassword, hash, callBack) {
-	bcrypt.compare(candidatePassword, hash, callBack);
+module.exports.comparePassword = (candidatePassword, hash, callBack) => {
+  bcrypt.compare(candidatePassword, hash, callBack);
 };
--- validations/post.js
+++ validations/post.js
@@ -2,9 +2,9 @@ const { check } = require('express-validator/check');
 
 module.exports.validations = [
   check('title').not()
-  .isEmpty().
-  withMessage('Title should not be empty.'),
+    .isEmpty()
+    .withMessage('Title should not be empty.'),
   check('description').not()
-  .isEmpty()
-  .withMessage('Description should not be empty.'),
+    .isEmpty()
+    .withMessage('Description should not be empty.'),
 ];

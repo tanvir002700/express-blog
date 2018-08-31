0a3855c670a0f1240368a34f4509194818241604
tanvir hasan <tanvir002700@gmail.com>
2018-08-31 20:58:10 +0600

    add signup page render test

---
+++ test/test.js
@@ -0,0 +1,19 @@
+var request = require('supertest');
+var app = require('../app');
+
+describe("Signup page", function() {
+  it("render Signup page", function(done) {
+    request(app).get('/users/new')
+      .expect(200)
+      .expect('Content-Type', 'text/html; charset=utf-8')
+      .expect(hasPreviousAndNextKeys)
+      .end(done);
+  });
+});
+
+function hasPreviousAndNextKeys(res) {
+  if (!res.text.includes('Email')) throw new Error("missing Email");
+  if (!res.text.includes('Username')) throw new Error("missing Username");
+  if (!res.text.includes('Password')) throw new Error("missing Password");
+  if (!res.text.includes('Password Confirmation')) throw new Error("missing Password Confirmation");
+}

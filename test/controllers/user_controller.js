a6d7eb844e9124e59ff656c475f8bbd6ffab1f96
tanvir hasan <tanvir002700@gmail.com>
2018-09-14 18:53:50 +0600

    add test for email validation in signup page

--- test/controllers/user_controller.js
+++ test/controllers/user_controller.js
@@ -19,7 +19,13 @@ function hasLoginElement(res) {
   if (!res.text.includes('Password')) throw new Error('missing Password');
 }
 
-describe('Signup page', () => {
+function hasSignUpError(res) {
+  console.log(res.text);
+  if(!res.text.includes('Email should not be empty.')) throw new Error('missing email presence validation.');
+  if(!res.text.includes('Is not Valid Email')) throw new Error('mission email format validation.');
+}
+
+describe('Rendering Signup page', () => {
   beforeEach(() => {
     setTimeout(() => flushAllTables(), 1000);
   });
@@ -53,6 +59,24 @@ describe('Signup page', () => {
   });
 });
 
+
+describe('Validate User Registration', () => {
+  it('register new user', (done) => {
+    request(app).get('/users/new')
+      .end(function(err, res){
+        dom = new JSDOM(res.text);
+        csrf = dom.window.document.getElementsByName('_csrf')[0].value;
+        request(app).post('/users/create')
+          .set('cookie', res.headers['set-cookie'])
+          .send({
+            _csrf: csrf
+          })
+          .expect(200)
+          .expect(hasSignUpError)
+          .end(done);
+      });
+  });
+});
 
 describe('Login page', () => {
   it('render Login page', (done) => {

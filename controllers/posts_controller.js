6c2d4ea12f9aae7a32a9bcde75b6e5458f0d0cef
tanvir hasan <tanvir002700@gmail.com>
2018-09-25 22:04:31 +0600

    fix lint for posts controller

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,80 +1,78 @@
 const express = require('express');
-const passport = require('passport');
-const { check, validationResult } = require('express-validator/check');
-const router = express.Router();
 const Post = require('../models/post');
 const { validations } = require('../validations/post');
 const { validationCheckForCreate, validationCheckForUpdate } = require('../middlewares/post');
 
+const router = express.Router();
 
-router.get('/', function(req, res, next) {
-    Post.all(null, function(err, posts) {
-        if(err) {
-            res.redirect('/');
-        } else {
-            res.render('posts/index', {posts: posts.rows, csrfToken: req.csrfToken()})
-        }
-    });
+
+router.get('/', (req, res) => {
+  Post.all(null, (err, posts) => {
+    if (err) {
+      res.redirect('/');
+    } else {
+      res.render('posts/index', { posts: posts.rows, csrfToken: req.csrfToken() });
+    }
+  });
 });
 
-router.get('/new', function(req, res, next) {
-    res.render('posts/new', { csrfToken: req.csrfToken() });
+router.get('/new', (req, res) => {
+  res.render('posts/new', { csrfToken: req.csrfToken() });
 });
 
-router.post('/create', validations, validationCheckForCreate, function(req, res, next) {
-    Post.create(req.body, function(err, result) {
-        if(err) {
-            res.render('posts/new', { csrfToken: req.csrfToken() });
-        } else {
-            res.redirect('/posts');
-        }
-    });
+router.post('/create', validations, validationCheckForCreate, (req, res) => {
+  Post.create(req.body, (err, result) => {
+    if (err) {
+      res.render('posts/new', { csrfToken: req.csrfToken() });
+    } else if (result) {
+      res.redirect('/posts');
+    }
+  });
 });
 
-router.get('/:id', function(req, res, next) {
-    const id = req.params.id;
-    Post.findById(id, function(err, post) {
-        if(err) {
-            res.render('/');
-        } else {
-            res.render('posts/show', {post: post.rows[0]});
-        }
-    });
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+  Post.findById(id, (err, post) => {
+    if (err) {
+      res.render('/');
+    } else {
+      res.render('posts/show', { post: post.rows[0] });
+    }
+  });
 });
 
-router.get('/:id/edit', function(req, res, next) {
-    const id = req.params.id;
-    Post.findById(id, function(err, post) {
-        if(err) {
-            res.render('/'+id+'/edit');
-        } else {
-            res.render('posts/edit', {post: post.rows[0], csrfToken: req.csrfToken()});
-        }
-    });
+router.get('/:id/edit', (req, res) => {
+  const { id } = req.params;
+  Post.findById(id, (err, post) => {
+    if (err) {
+      res.render(`/${id}/edit`);
+    } else {
+      res.render('posts/edit', { post: post.rows[0], csrfToken: req.csrfToken() });
+    }
+  });
 });
 
-router.put('/:id/update', validations, validationCheckForUpdate, function(req, res, next) {
-    const id = req.params.id;
-    Post.update(id, req.body, function(err, result) {
-        if(err) {
-            res.render('/'+id+'/edit');
-        } else {
-            res.redirect('/posts');
-            return;
-        }
-    });
+router.put('/:id/update', validations, validationCheckForUpdate, (req, res) => {
+  const { id } = req.params;
+  Post.update(id, req.body, (err, result) => {
+    if (err) {
+      res.render(`/${id}/edit`);
+    } else if (result) {
+      res.redirect('/posts');
+    }
+  });
 });
 
 
-router.delete('/:id', function(req, res, next) {
-    const id = req.params.id;
-    Post.delete(id, function(err, result) {
-        if(err) {
-            res.redirect('/posts');
-        } else {
-            res.redirect('/posts');
-        }
-    });
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+  Post.delete(id, (err, result) => {
+    if (err) {
+      res.redirect('/posts');
+    } else if (result) {
+      res.redirect('/posts');
+    }
+  });
 });
 
 module.exports = router;

b49338a3f39bb5a73885447c731b9f977c270d92
tanvir hasan <tanvir002700@gmail.com>
2018-09-26 23:11:37 +0600

    upgrade schema

--- db/schema.js
+++ db/schema.js
@@ -1,13 +1,19 @@
 const client = require('./client');
 
-queries = [
-    'CREATE TABLE IF NOT EXISTS users(id uuid, email text, username text, password text, PRIMARY KEY(id, email, username))',
-    'CREATE TABLE IF NOT EXISTS posts(id uuid PRIMARY KEY, title text, description text)'
+const queries = [
+  'CREATE TABLE IF NOT EXISTS users(id uuid, email text, username text, password text, PRIMARY KEY(id, email, username))',
+  'CREATE TABLE IF NOT EXISTS posts(id uuid PRIMARY KEY, title text, description text)',
 ];
 
-queries.forEach(function(query) {
-    client.execute(query, [], function(err, res) {
-        console.log("Run query.........");
-        console.log(query);
-    });
+let success = 0;
+
+queries.forEach((query) => {
+  client.execute(query, [], (_, res) => {
+    console.log('Run query.........');
+    console.log(query);
+    if (res) success += 1;
+    if (queries.length === success) {
+      process.exit(0);
+    }
+  });
 });

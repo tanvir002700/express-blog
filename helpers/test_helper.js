94a76d9d62b1554fe7461ffdf97f4da34831c6d0
tanvir hasan <tanvir002700@gmail.com>
2018-09-28 12:56:14 +0600

    refactor test helper

--- helpers/test_helper.js
+++ helpers/test_helper.js
@@ -3,14 +3,14 @@ const Client = require('../db/client');
 const config = require('../config').database;
 
 const truncateTable = (name) => {
-  Client.execute("TRUNCATE "+name, [], function(err, res) {
-    process.exit(0)
+  Client.execute(`TRUNCATE ${name}`, [], (err, res) => {
+    if (res) process.exit(0);
   });
 };
 
 const allTablesName = (callback) => {
-  Client.execute(query.Cassandra.ALL_TABLES, [config.keyspace], function(err, res) {
-    res.rows.forEach(function(item){
+  Client.execute(query.Cassandra.ALL_TABLES, [config.keyspace], (err, res) => {
+    res.rows.forEach((item) => {
       callback(item.table_name);
     });
   });

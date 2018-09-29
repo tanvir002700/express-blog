db48b4a7dabde2cf081e542f7c3468f76820c5db
tanvir hasan <tanvir002700@gmail.com>
2018-09-29 21:14:18 +0600

    refactor user middlewares

--- middlewares/user.js
+++ middlewares/user.js
@@ -1,18 +1,18 @@
-const User = require('../models/user');
 const { validationResult } = require('express-validator/check');
+const User = require('../models/user');
 
-module.exports.validationCheckForRegistration = function(req, res, next) {
-    var errors = validationResult(req);
-    if(!errors.isEmpty()) {
-      res.render('users/new', { csrfToken: req.csrfToken(), errors: errors.array() });
-    } else {
-      next();
-    }
+module.exports.validationCheckForRegistration = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    res.render('users/new', { csrfToken: req.csrfToken(), errors: errors.array() });
+  } else {
+    next();
+  }
 };
 
-module.exports.checkDuplicateUserByEmail = function(req, res, next) {
-  User.findByEmail(req.body.email, function(err, result) {
-    if(result.rows.length || err) {
+module.exports.checkDuplicateUserByEmail = (req, res, next) => {
+  User.findByEmail(req.body.email, (err, result) => {
+    if (result.rows.length || err) {
       req.flash('error', 'User already exist with this email.');
       res.render('users/new', { csrfToken: req.csrfToken() });
     } else {
@@ -21,9 +21,9 @@ module.exports.checkDuplicateUserByEmail = function(req, res, next) {
   });
 };
 
-module.exports.checkDuplicateUserByUserName = function(req, res, next) {
-  User.findByUsername(req.body.username, function(err, result) {
-    if(result.rows.length || err) {
+module.exports.checkDuplicateUserByUserName = (req, res, next) => {
+  User.findByUsername(req.body.username, (err, result) => {
+    if (result.rows.length || err) {
       req.flash('error', 'User already exist with this username.');
       res.render('users/new', { csrfToken: req.csrfToken() });
     } else {
@@ -32,8 +32,8 @@ module.exports.checkDuplicateUserByUserName = function(req, res, next) {
   });
 };
 
-module.exports.checkPasswordConfirmation = function(req, res, next) {
-  if(req.body.password === req.body.password_confirmation) {
+module.exports.checkPasswordConfirmation = (req, res, next) => {
+  if (req.body.password === req.body.password_confirmation) {
     next();
   } else {
     req.flash('error', 'Password not Match.');

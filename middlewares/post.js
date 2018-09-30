e203af24157ddfd05e5ee0fac24a076809c6851b
tanvir hasan <tanvir002700@gmail.com>
2018-09-30 21:15:07 +0600

    refactor posts middlewares

--- middlewares/post.js
+++ middlewares/post.js
@@ -1,26 +1,27 @@
 const { validationResult } = require('express-validator/check');
+const Post = require('../models/post');
 
-module.exports.validationCheckForCreate = function(req, res, next) {
-    var errors = validationResult(req);
-    if(!errors.isEmpty()) {
-      res.render('posts/new', { csrfToken: req.csrfToken(), errors: errors.array() });
-    } else {
-      next();
-    }
+module.exports.validationCheckForCreate = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    res.render('posts/new', { csrfToken: req.csrfToken(), errors: errors.array() });
+  } else {
+    next();
+  }
 };
 
-module.exports.validationCheckForUpdate = function(req, res, next) {
-    var errors = validationResult(req);
-    const id = req.params.id;
-    if(!errors.isEmpty()) {
-        Post.findById(id, function(err, post) {
-            if(err) {
-                res.render('/'+id+'/edit');
-            } else {
-                res.render('posts/edit', {post: post.rows[0], csrfToken: req.csrfToken(), errors: errors.array()});
-            }
-        });
-    } else {
-      next();
-    }
+module.exports.validationCheckForUpdate = (req, res, next) => {
+  const errors = validationResult(req);
+  const { id } = req.params;
+  if (!errors.isEmpty()) {
+    Post.findById(id, (err, post) => {
+      if (err) {
+        res.render(`/id/${id}/edit`);
+      } else {
+        res.render('posts/edit', { post: post.rows[0], csrfToken: req.csrfToken(), errors: errors.array() });
+      }
+    });
+  } else {
+    next();
+  }
 };

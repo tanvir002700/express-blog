37dc0c88e32d90e3f61ca1a54cd6034d0d39084f
tanvir hasan <tanvir002700@gmail.com>
2018-10-01 00:42:16 +0600

    refactor auth middlewares

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,47 +1,48 @@
 const bcrypt = require('bcryptjs');
-var passport = require('passport');
-var LocalStrategy = require('passport-local').Strategy;
-var User = require('../models/user');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const User = require('../models/user');
 
-const passwordMatchingCallback = function(err, isMatch) {
-    if(err) throw err;
-    if(isMatch) {
-        return this.done(null, user);
-    } else {
-        return this.done(null, false, { message: 'Invalid Password' });
-    }
-}
-
-const authenticationCallback = function(err, result) {
-    if(err) throw err;
-    if(!result.rows.length) {
-        return this.done(null, false, { message: 'Unknown User' });
-    }
-    user = result.rows[0];
-    bcrypt.compare(this.password, user.password, passwordMatchingCallback.bind(this));
+const passwordMatchingCallback = (err, isMatch) => {
+  if (err) throw err;
+  if (isMatch) {
+    return this.done(null, this.user);
+  }
+  return this.done(null, false, { message: 'Invalid Password' });
 };
 
-passport.use(new LocalStrategy(function(username, password, done) {
-    this.password = password;
-    this.done = done;
-    User.findByUsername(username, authenticationCallback.bind(this));
+const authenticationCallback = (err, result) => {
+  const that = this;
+  if (err) throw err;
+  if (!result.rows.length) {
+    return that.done(null, false, { message: 'Unknown User' });
+  }
+  [this.user] = result.rows;
+  this.done = that.done;
+  return bcrypt.compare(that.password, this.user.password, passwordMatchingCallback.bind(this));
+};
+
+passport.use(new LocalStrategy((username, password, done) => {
+  this.password = password;
+  this.done = done;
+  User.findByUsername(username, authenticationCallback.bind(this));
 }));
 
-passport.serializeUser(function(user, done) {
-    done(null, user.id);
+passport.serializeUser((user, done) => {
+  done(null, user.id);
 });
 
-passport.deserializeUser(function(id, done) {
-    User.findById(id, function(err, result) {
-        done(err, result.rows[0]);
-    });
+passport.deserializeUser((id, done) => {
+  User.findById(id, (err, result) => {
+    done(err, result.rows[0]);
+  });
 });
 
 module.exports.passport = passport;
 
-module.exports.isLoggedIn = function(req, res, next) {
-    if(req.isAuthenticated()) {
-        return next();
-    }
-    return res.redirect('users/login');
-}
+module.exports.isLoggedIn = (req, res, next) => {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  return res.redirect('users/login');
+};

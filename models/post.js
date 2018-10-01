3fedcb0532690a0195fac32cecddf1e10350863e
tanvir hasan <tanvir002700@gmail.com>
2018-10-01 20:55:19 +0600

    refactor post models

--- models/post.js
+++ models/post.js
@@ -2,22 +2,22 @@ const client = require('../db/client');
 const Query = require('../db/query');
 
 
-module.exports.all = function(params, callBack) {
-    client.execute(Query.Post.ALL, [], callBack);
+module.exports.all = (params, callBack) => {
+  client.execute(Query.Post.ALL, [], callBack);
 };
 
-module.exports.create = function(params, callBack) {
-    client.execute(Query.Post.CREATE, [params.title, params.description], callBack);
+module.exports.create = (params, callBack) => {
+  client.execute(Query.Post.CREATE, [params.title, params.description], callBack);
 };
 
-module.exports.findById = function(id, callBack) {
-    client.execute(Query.Post.FIND_BY_ID, [id], callBack);
+module.exports.findById = (id, callBack) => {
+  client.execute(Query.Post.FIND_BY_ID, [id], callBack);
 };
 
-module.exports.update = function(id, params, callBack) {
-    client.execute(Query.Post.UPDATE, [params.title, params.description, id], callBack);
+module.exports.update = (id, params, callBack) => {
+  client.execute(Query.Post.UPDATE, [params.title, params.description, id], callBack);
 };
 
-module.exports.delete = function(id, callBack) {
-    client.execute(Query.Post.DELETE, [id], callBack);
+module.exports.delete = (id, callBack) => {
+  client.execute(Query.Post.DELETE, [id], callBack);
 };
